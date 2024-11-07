0b78c7884a6659b441d9e445c6aac3eb0b6b9556
fajri1997 <f.a.f.s.m.alhusaini@edu.salford.ac.uk>
2024-11-07 03:17:09 +0000

    DONE

--- src/IsLeapYear/Main.java
+++ src/IsLeapYear/Main.java
@@ -1,7 +1,6 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+package IsLeapYear;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
-import java.util.Date;
 import java.util.Scanner;
 
 
